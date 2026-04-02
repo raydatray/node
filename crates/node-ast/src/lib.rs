db48afb0a9412cf918db0e735cf013868c49acbb
Ray Liu <raydatray@gmail.com>
2026-04-02 16:12:33 -0400

    [01-ast][6/n] impl project
    Summary:
    
    Test Plan:

--- crates/node-ast/src/lib.rs
+++ crates/node-ast/src/lib.rs
@@ -1,6 +1,9 @@
 pub mod ast;
 pub mod error;
 pub mod parser;
+pub mod project;
+
 pub use ast::*;
 pub use error::ParseError;
 pub use parser::{parse_entity, parse_entity_file};
+pub use project::load_project;
